3399d97babc252b3b25a13b5539179750e83077b
Erzbir <erzbir@mail.com>
2024-11-25 14:48:25 +0800

    [test]

---
+++ albaz-plugin/src/test/java/com/erzbir/albaz/plugin/PluginHandleTest.java
@@ -0,0 +1,21 @@
+package com.erzbir.albaz.plugin;
+
+import com.erzbir.albaz.plugin.exception.PluginNotFoundException;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class PluginHandleTest {
+
+    @Test
+    void unload() throws PluginNotFoundException {
+        JavaPluginManager pluginManager = new JavaPluginManager();
+        pluginManager.useServiceLoader(true);
+        pluginManager.loadPlugins();
+        List<PluginHandle> pluginHandles = pluginManager.getPluginHandles();
+        for (PluginHandle pluginHandle : pluginHandles) {
+            pluginHandle.unload();
+
+        }
+    }
+}
