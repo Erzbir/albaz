5777d189c22db9fb8b7fe5a87802a060e1d0a2c9
Erzbir <erzbir@mail.com>
2024-12-09 21:23:20 +0800

    [doc]

--- albaz-plugin/src/main/java/com/erzbir/albaz/plugin/loader/PluginClassLoader.java
+++ albaz-plugin/src/main/java/com/erzbir/albaz/plugin/loader/PluginClassLoader.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2012-present the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.erzbir.albaz.plugin.loader;
 
 import com.erzbir.albaz.logging.Log;
@@ -23,7 +8,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-
+/**
+ * @author Erzbir
+ * @since 1.0.0
+ */
 public class PluginClassLoader extends URLClassLoader {
 
     private static final Log log = LogFactory.getLog(PluginClassLoader.class);
